4633d94ed227a064d8fce08e4faee637d4b6c47f
Siva Kandasamy JANKAN <sivajankan@gmail.com>
2012-10-08 21:50:39 -0400

    first addition

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stockslist2::Application.config.secret_token = '8c5413a667f7a59cec254d36c99ed46b7eed017a546510826205abb315f5ff4d81eea3c821be3c79ece8a8d7f73fde222b4d95a65911d30c50265c70100b4d9d'
