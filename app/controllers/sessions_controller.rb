bd1926ea93acc92695be5c3011329d5c5e17f7c0
Siva Kandasamy JANKAN <sivajankan@gmail.com>
2013-06-01 10:33:36 -0400

    omni auth partial

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
     unless @user = User.auth_find_from_hash(user)
       # Create a new user or add an auth to existing user, depending on
       # whether there is already a user signed in.
+      debugger
       @user = User.auth_create_from_hash(user, current_user)
     end
     # Log the authorizing user in.
@@ -21,7 +22,7 @@ class SessionsController < ApplicationController
     #@authentication = current_user.authentications.find(params[:id])
     #@authentication.destroy
     @user.provider = ''
-    @user.save!
+    @user.save
     flash[:notice] = "Successfully destroyed authentication."
     redirect_to authentications_url
   end
